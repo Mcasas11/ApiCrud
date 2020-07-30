d798ab654ec95b7bcaceb5b2e97aea4a53c2abcd
marcos.casas.f@outlook.com <marcos.casas.f@outlook.com>
2020-07-30 12:04:12 -0400

    CRUD working

---
+++ ApiCrud/Migrations/20200729171202_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApiCrud.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    personId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    personName = table.Column<string>(nullable: true),
+                    personLastName = table.Column<string>(nullable: true),
+                    personAge = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.personId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
---
+++ ApiCrud/Models/CrudDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCrud.Models
+{
+    public class CrudDbContext : DbContext
+    {
+        public CrudDbContext() { }
+        public CrudDbContext(DbContextOptions<CrudDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Person> Persons { get; set; }
+
+    }
+    public class Person
+    {
+        [Key]
+        public int personId { get; set; }
+        public string personName { get; set; }
+        public string personLastName { get; set; }
+        public string personAge { get; set; }
+    }
+}
